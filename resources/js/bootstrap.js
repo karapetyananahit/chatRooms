fb4babd923c7a2773aaa831e87e7dbd68d3a515b
karapetyananahit <anahitkarapetyan075@hmail.com>
2022-04-03 00:29:36 +0400

    create a chat room

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,9 @@
 window._ = require('lodash');
 
+try {
+    require('bootstrap');
+} catch (e) {}
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -15,6 +19,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
+
 
 import Echo from 'laravel-echo';
 
