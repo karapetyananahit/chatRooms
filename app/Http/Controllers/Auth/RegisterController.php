108252be801cef0dc3415f21f34f2f0f26d09943
karapetyananahit <anahitkarapetyan075@hmail.com>
2022-04-03 19:41:52 +0400

    create surname and gender fields in register page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'gender'=> ['required', 'string'],
+            'surname' => ['required', 'string', 'max:255'],
         ]);
     }
 
@@ -68,6 +70,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'gender' => $data['gender'],
+            'surname' => $data['surname'],
         ]);
     }
 }
